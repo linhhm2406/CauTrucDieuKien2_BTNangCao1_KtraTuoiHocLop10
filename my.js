c9fd8a6bb92a3b3ddb110c7307dc2501e833dbec
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:06:00 +0700

    add file

---
+++ my.js
@@ -0,0 +1,7 @@
+let age=parseInt(prompt('Nhap vao so tuoi'));
+
+if(age<16) {
+    document.write('Hoc sinh chua du tuoi hoc lop 10');
+}else {
+    document.write('Hoc sinh du tuoi hoc lop 10');
+}
